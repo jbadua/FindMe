284a7f0b3b2418b37aa03fed16b2618a26b6b0c7
Jason Badua <jbadua@ucla.edu>
2015-04-20 22:01:32 -0700

    Initial Commit

---
+++ FindMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FindMe
+//
+//  Created by Jason Badua on 4/20/15.
+//  Copyright (c) 2015 CS 117. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FindMe/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FindMe
+//
+//  Created by Jason Badua on 4/20/15.
+//  Copyright (c) 2015 CS 117. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
