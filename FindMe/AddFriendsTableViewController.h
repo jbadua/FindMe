b08865ee5aaedb673a87865afb4f639710676367
Jason Badua <jbadua@ucla.edu>
2015-05-26 16:02:27 -0700

    Users can add friends

---
+++ FindMe/AddFriendsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddFriendsTableViewController.h
+//  FindMe
+//
+//  Created by Jason Badua on 5/18/15.
+//  Copyright (c) 2015 CS 117. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddFriendsTableViewController : UITableViewController
+
+@end
