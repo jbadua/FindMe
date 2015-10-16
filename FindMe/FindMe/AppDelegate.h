c948b8cac213ff35a32a71299a6600b129d5cae1
Jason Badua <jbadua@ucla.edu>
2015-10-15 19:14:39 -0700

    Start from blank project

---
+++ FindMe/FindMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FindMe
+//
+//  Created by Jason Badua on 10/15/15.
+//  Copyright © 2015 Jason Badua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FindMe/FindMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FindMe
+//
+//  Created by Jason Badua on 10/15/15.
+//  Copyright © 2015 Jason Badua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
